015c17d3c017ea5eb8f749f2b2afc5be3b9c44e5
Aryaman2912 <aryavnps@gmail.com>
2020-11-24 14:07:08 +0530

    add welcome message functionality

---
+++ bot/bot.py
@@ -0,0 +1,32 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds,name=GUILD)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+    # print(guild.members)
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+client.run(TOKEN)
