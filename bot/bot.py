547141558943e5cd4a7c3c089662cb3c56e4d200
Aryaman2912 <aryavnps@gmail.com>
2020-11-24 15:13:45 +0530

    respond to messages

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 import discord
 from dotenv import load_dotenv
 
@@ -29,4 +29,31 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my Discord server!'
     )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    lols = [
+        "Arjun is a pet dog that bites all strangers",
+        "Aditi is a very very boring cousin",
+        "Chintu is a field marshal and a war hero",
+        "Amogh is a douchebag and always says 'hurts man'",
+        "Arya is my master",
+    ]
+    if message.content.lower() == 'arjun':
+        response = lols[0]
+        await message.channel.send(response)
+    if message.content.lower() == 'chintu':
+        response = lols[2]
+        await message.channel.send(response)
+    if message.content.lower() =='aditi':
+        response = lols[1]
+        await message.channel.send(response)
+    if message.content.lower() == 'amogh':
+        response = lols[3]
+        await message.channel.send(response)
+    if message.content.lower() == 'arya':
+        response = lols[4]
+        await message.channel.send(response)
 client.run(TOKEN)
