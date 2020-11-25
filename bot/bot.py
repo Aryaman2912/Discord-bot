c6d145000b59f7ca12eb0dd79350bbaaf2af67a8
Aryaman2912 <aryavnps@gmail.com>
2020-11-25 18:51:33 +0530

    add create-channel function

--- bot/bot.py
+++ bot/bot.py
@@ -14,9 +14,11 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.all()
-client = discord.Client(intents=intents)
+# client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!',intents=intents)
 
+
+# Print members to stdout when connected
 @bot.event
 async def on_ready():
     guild = discord.utils.get(bot.guilds,name=GUILD)
@@ -28,16 +30,19 @@ async def on_ready():
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
-    # print(guild.members)
 
+# Send a dm to a new joiner
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my Discord server!'
     )
+    await bot.process_commands(member)
 
-@bot.event
+
+# Respond to specific messages on the server
+@bot.listen('on_message')
 async def on_message(message):
     if message.author == bot.user:
         return
@@ -60,43 +65,56 @@ async def on_message(message):
     }
     response = lols[d[message.content.lower()]]
     await message.channel.send(response)
+    # await bot.process_commands(message)
 
+# Fetch inspiration quotes 
 URL = 'https://www.brainyquote.com/topics/inspirational-quotes'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content,'html.parser')
 results = soup.find(id='quotesList')
 quotes = results.find_all('a',class_='b-qt')
-quoteslist = []
+inspiration_list = []
 for quote in quotes:
-    quoteslist.append(quote.text)
-    # print(quote.text)
-# print(len(quoteslist))
-bot = commands.Bot(command_prefix='!',intents=intents)
+    inspiration_list.append(quote.text)
 
-@bot.command(name='inspiration',help='Responds with an inspirational quote when asked')
-async def inspiration(ctx):
-    response = random.choice(quoteslist)
-    await ctx.send(response)
-
+# Fetch motivational quotes
 URL1 = 'https://www.brainyquote.com/topics/motivational-quotes'
 page = requests.get(URL1)
 soup = BeautifulSoup(page.content,'html.parser')
 results = soup.find(id='quotesList')
 quotes = results.find_all('a',class_='b-qt')
-quoteslist1 = []
+motivation_list = []
 for quote in quotes:
-    quoteslist1.append(quote.text)
-    # print(quote.text)
-# print(len(quoteslist1))
-bot = commands.Bot(command_prefix='!',intents=intents)
+    motivation_list.append(quote.text)
 
+# Respond to '!inspiration' command
+@bot.command(name='inspiration',help='Responds with an inspirational quote when asked')
+async def inspiration(ctx):
+    response = random.choice(inspiration_list)
+    await ctx.send(response)
+
+# Respond to '!motivation' command
 @bot.command(name='motivation',help='Responds with a motivational quote when asked')
 async def motivation(ctx):
-    response = random.choice(quoteslist1)
+    response = random.choice(motivation_list)
     await ctx.send(response)
     
+# Create new channel
+@bot.command(name='create-channel')
+@commands.has_role('admin')
+async def create_channel(ctx,channel_name = 'new'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels,name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
 
-
+# Inform user of wrong permissions
+@bot.event
+async def on_command_error(ctx,error):
+    if isinstance(error,commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command')
+# Error handling
 @bot.event
 async def on_error(event, *args, **kwargs):
     with open('err.log','a') as f:
