f56ed2c53bf30bd290f10f6033f5d02d78c77f07
Aryaman2912 <aryavnps@gmail.com>
2020-12-04 01:23:14 +0530

    Kick works

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import random
 import discord
 import selenium
@@ -8,7 +9,7 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 from discord.ext import commands
-
+from discord.ext.commands import has_permissions,CheckFailure,BadArgument
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -46,7 +47,7 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == bot.user:
         return
-    response = "at your service"
+    response = "At your service."
     if message.content.lower() == 'jarvis':
         await message.channel.send(response)
 
@@ -70,20 +71,36 @@ motivation_list = []
 for quote in quotes:
     motivation_list.append(quote.text)
 
+# Fetch attitude quotes 
+URL = 'https://www.brainyquote.com/topics/attitude-quotes'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content,'html.parser')
+results = soup.find(id='quotesList')
+quotes = results.find_all('a',class_='b-qt')
+attitude_list = []
+for quote in quotes:
+    attitude_list.append(quote.text)
+
 # Respond to '!inspiration' command
-@bot.command(name='inspiration',help='Responds with an inspirational quote when asked')
+@bot.command(name='inspiration',help='Responds with an inspirational quote')
 async def inspiration(ctx):
     response = random.choice(inspiration_list)
     await ctx.send(response)
 
 # Respond to '!motivation' command
-@bot.command(name='motivation',help='Responds with a motivational quote when asked')
+@bot.command(name='motivation',help='Responds with a motivational quote')
 async def motivation(ctx):
     response = random.choice(motivation_list)
     await ctx.send(response)
     
+# Respond to '!attitude' command
+@bot.command(name='attitude',help='Responds with a quote about attitude')
+async def attitude(ctx):
+    response = random.choice(attitude_list)
+    await ctx.send(response)
+
 # Create new channel
-@bot.command(name='create-channel')
+@bot.command(name='create-channel',help='creates a new text channel')
 @commands.has_role('admin')
 async def create_channel(ctx,channel_name = 'new'):
     guild = ctx.guild
@@ -92,11 +109,29 @@ async def create_channel(ctx,channel_name = 'new'):
         print(f'Creating a new channel: {channel_name}')
         await guild.create_text_channel(channel_name)
 
-# Inform user of wrong permissions
-@bot.event
-async def on_command_error(ctx,error):
-    if isinstance(error,commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command')
+# Kick members
+@bot.command(pass_context=True,name='kick',help='kick a member')
+@commands.has_permissions(kick_members=True)
+async def kick(ctx,username: discord.Member):
+    if username.guild_permissions.administrator:
+        await ctx.send("Target is an admin")
+    else:
+        await ctx.guild.kick(username)
+        await ctx.send(f"Kicked {username}")
+@kick.error
+async def kick_error(ctx,error):
+    try:
+        if isinstance(error,commands.errors.BadArgument):
+            print("Here1")
+            await ctx.send("Invalid username")
+        elif isinstance(error,commands.errors.MissingPermissions):
+            print("Here2")
+            await ctx.send("You do not have the correct permissions for this command")
+    except:
+        print("Here4")
+        print(sys.exc_info())
+        await ctx.send(sys.exc_info())
+
 # Error handling
 @bot.event
 async def on_error(event, *args, **kwargs):
@@ -105,5 +140,4 @@ async def on_error(event, *args, **kwargs):
             f.write(f'Unhandled message: {args[0]}\n')
         else:
             raise
-# client.run(TOKEN)
 bot.run(TOKEN)
